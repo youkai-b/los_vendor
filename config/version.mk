DerpFest-AOSP/vendor_derp

config: Don't use UTC for versioning
* use actual local time of the machine the build was produced on instead

config/version.mk
@@ -15,9 +15,9 @@
 
 # Versioning System
 ifeq ($(DERP_VERSION_APPEND_TIME_OF_DAY),true)
-    BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
+    BUILD_DATE := $(shell date +%Y%m%d-%H%M)
 else
-    BUILD_DATE := $(shell date -u +%Y%m%d)
+    BUILD_DATE := $(shell date +%Y%m%d)
 endif
 
 TARGET_PRODUCT_SHORT := $(subst derp_,,$(DERP_BUILDTYPE))
@@ -26,7 +26,7 @@ DERP_BUILDTYPE ?= Community
 DERP_STATUS := Beta
 DERP_BUILD_VERSION := $(PLATFORM_VERSION)
 DERP_VERSION := $(DERP_BUILD_VERSION)-$(DERP_BUILDTYPE)-$(DERP_STATUS)-$(DERP_BUILD)-$(BUILD_DATE)
-ROM_FINGERPRINT := DerpFest/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%H%M)
+ROM_FINGERPRINT := DerpFest/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date +%H%M)
 
 ifeq ($(DERP_BUILDTYPE), CI)
     BUILD_KEYS := release-keys

youkai-b/los_vendor

config/version.mk
@@ -2,9 +2,9 @@ PRODUCT_VERSION_MAJOR = 22
 PRODUCT_VERSION_MINOR = 2
 
 ifeq ($(LINEAGE_VERSION_APPEND_TIME_OF_DAY),true)
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d_%H%M)
+    LINEAGE_BUILD_DATE := $(shell date +%Y%m%d_%H%M)
 else
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d)
+    LINEAGE_BUILD_DATE := $(shell date +%Y%m%d)
 endif
 
 # Set LINEAGE_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat